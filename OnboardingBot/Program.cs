c75611d1286ae53971a70335864dd838f7384ad6
CryF1sh <glebbogomolov04@gmail.com>
2023-05-11 15:55:20 +0300

    Доделал регистрацию

--- OnboardingBot/Program.cs
+++ OnboardingBot/Program.cs
@@ -11,6 +11,8 @@ using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types.InlineQueryResults;
 using System.Text;
 using System.Net.Http;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using Server.Entities;
 
 namespace OnboardingBot
 {
@@ -19,71 +21,87 @@ namespace OnboardingBot
         static HttpClient HttpClient = new HttpClient();
         static ITelegramBotClient BotClient = new TelegramBotClient("5654249846:AAHU6xVbyc8vGMpODDM9h9kAfS4khjBUgig");
 
-        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        public static async Task HandleUpdatesAsync(ITelegramBotClient botClient, Telegram.Bot.Types.Update update, CancellationToken cancellationToken)
         {
-
-            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
-            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            if (update.Type == UpdateType.Message && update?.Message?.Text != null)
             {
-                var message = update.Message;
-                if (message.Text.ToLower() == "/start")
-                {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Привет! Я Чат-бот путеводитель твоих первых шагов в нашем техникуме, моя задача помогать новым студентам адаптироваться к новым условиям!");
-                    //Получение JSON данных из api
-                    HttpResponseMessage response = await HttpClient.GetAsync("api/Directions");
-                    if (response == null)
-                    {
-                        await botClient.SendTextMessageAsync(message.Chat.Id, "Ошибка! Не удалось найти направления!");
-                        return;
-                    }
-                    string responseContent = await response.Content.ReadAsStringAsync();
-                    //Десирилизация json в список
-                    List<DirectionModel> directions = JsonConvert.DeserializeObject<List<DirectionModel>>(responseContent);
+                await HandleMessage(botClient, update.Message);
+                return;
+            }
 
-                    List<InlineKeyboardButton> directionButtons = new List<InlineKeyboardButton>();
-
-                    //Перебираем каждое направление из созданного списка
-                    foreach (DirectionModel direction in directions)
-                    {
-                        //Добавляем кнопку для каждого направления
-                        InlineKeyboardButton button = InlineKeyboardButton.WithCallbackData(direction.NameDirection, "direction_" + direction.Id.ToString());
-                        directionButtons.Add(button);
-                    }
-                    InlineKeyboardMarkup inlineKeyboardDirection = new InlineKeyboardMarkup(directionButtons);
-
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Чтобы продолжить, мне необходимо задать тебе вопрос:\nНа каком направлении ты обучаешься?", replyMarkup: inlineKeyboardDirection);
-
-                    
-
-                    return;
-                }
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Ошибка! Не удалось распознать команду!");
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                await HandleCallbackQuery(botClient, update.CallbackQuery, cancellationToken);
+                return;
             }
         }
-
-        public static async Task BotClient_OnCallbackQuery(ITelegramBotClient botClient, Update update, object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)
+        public static async Task HandleMessage(ITelegramBotClient botClient, Message message)
         {
-            var callbackQuery = e.CallbackQuery;
-            var buttonData = callbackQuery.Data;
-            var message = update.Message;
+            //Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            if (message.Text.ToLower() == "/start")
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Привет! Я Чат-бот путеводитель твоих первых шагов в нашем техникуме, моя задача помогать новым студентам адаптироваться к новым условиям!");
+                //Получение JSON данных из api
+                HttpResponseMessage response = await HttpClient.GetAsync("api/Directions");
+                if (response == null)
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Ошибка! Не удалось найти направления!");
+                    return;
+                }
+                string responseContent = await response.Content.ReadAsStringAsync();
+                //Десирилизация json в список
+                List<DirectionModel> directions = JsonConvert.DeserializeObject<List<DirectionModel>>(responseContent);
 
+                List<InlineKeyboardButton> directionButtons = new List<InlineKeyboardButton>();
+
+                //Перебираем каждое направление из созданного списка
+                foreach (DirectionModel direction in directions)
+                {
+                    //Добавляем кнопку для каждого направления
+                    InlineKeyboardButton button = InlineKeyboardButton.WithCallbackData(direction.NameDirection, "direction_" + direction.Id.ToString());
+                    directionButtons.Add(button);
+                }
+                InlineKeyboardMarkup inlineKeyboardDirection = new InlineKeyboardMarkup(directionButtons);
+
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Чтобы продолжить, мне необходимо задать тебе вопрос:\nНа каком направлении ты обучаешься?", replyMarkup: inlineKeyboardDirection);
+
+                return;
+            }
+            await botClient.SendTextMessageAsync(message.Chat.Id, "Ошибка! Не удалось распознать команду!");
+        }
+
+        public static async Task HandleCallbackQuery(ITelegramBotClient botClient, CallbackQuery callbackQuery, CancellationToken cancellationToken)
+        {
+            var buttonData = callbackQuery.Data;
             if (buttonData.StartsWith("direction_"))
             {
-                
-                HttpResponseMessage registrationResponse = await HttpClient.PostAsync("api/users", null);
+                string directionId = buttonData.Substring(10);
+
+                var userData = new
+                {
+                    id = callbackQuery.From.Id,
+                    TelegramID = callbackQuery.From.Id,
+                    DirectionID = directionId
+                };
+
+                var jsonPayload = JsonConvert.SerializeObject(userData);
+
+                var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage registrationResponse = await HttpClient.PostAsync("api/Users", content, cancellationToken);
 
                 if (registrationResponse.IsSuccessStatusCode)
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Регистрация прошла успешно! Вы выбрали направление - ");
+                    await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "Регистрация прошла успешно! Вы выбрали направление - " + directionId);
                 }
                 else
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Ошибка! Не удалось провести регистрацию! Попробуйте позже.");
+                    await botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "Ошибка! Не удалось провести регистрацию!");
                 }
             }
         }
 
-
+        
         public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
             // Некоторые действия
@@ -94,8 +112,6 @@ namespace OnboardingBot
         {
             Console.WriteLine("Bot launched: " + BotClient.GetMeAsync().Result.FirstName);
 
-            //string connectionString = "Host=localhost;Port=5432;Database=OnboardingBot;Username=onboardingbot;Password=onboardingbot";
-
             RunAsync().GetAwaiter().GetResult();
 
             var cts = new CancellationTokenSource();
@@ -104,7 +120,7 @@ namespace OnboardingBot
             {
                 AllowedUpdates = { }, // receive all update types
             };
-            BotClient.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, cancellationToken);
+            BotClient.StartReceiving(HandleUpdatesAsync, HandleErrorAsync, receiverOptions, cancellationToken);
             Console.ReadLine();
         }
         static async Task RunAsync()
